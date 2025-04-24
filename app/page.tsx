84f10505e95393655c506eee6519f8098d6b5cb1
TechFarmBR <pedroneto.f@hotmail.com>
2025-04-24 20:33:32 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Hero from '../components/Hero';
+import CTA from '../components/CTA';
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <CTA />
     </>
   );
 }
